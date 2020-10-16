ce16c05e67926b7b7f88d1e7b027855573346d65
zhangcn <15236674712@163.com>
2020-10-16 09:55:59 +0800

    这是新建的项目 v1.0

---
+++ src/main/java/com/baizhi/git_2004/Git2004Application.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_2004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2004Application.class, args);
+    }
+
+}
