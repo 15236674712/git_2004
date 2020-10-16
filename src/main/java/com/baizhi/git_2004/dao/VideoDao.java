90f8e63a3f2d80e65fc1933a31852962e19d03e0
zhangcn <15236674712@163.com>
2020-10-16 11:14:57 +0800

    小黄:视频模块基础功能实现完成

---
+++ src/main/java/com/baizhi/git_2004/dao/VideoDao.java
@@ -0,0 +1,10 @@
+package com.baizhi.git_2004.dao;
+
+import com.baizhi.git_2004.entity.Video;
+
+import java.util.List;
+
+public interface VideoDao {
+
+    List<Video> queryByLike(String content);
+}
---
+++ src/main/java/com/baizhi/git_2004/entity/Video.java
@@ -0,0 +1,7 @@
+package com.baizhi.git_2004.entity;
+
+public class Video {
+
+    private String id;
+    private String title;
+}
