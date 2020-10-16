eef70c1215e321000f889ec83313a43a8f185464
zhangcn <15236674712@163.com>
2020-10-16 11:12:42 +0800

    小黑:用户模块基础功能已经完成

---
+++ src/main/java/com/baizhi/git_2004/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.baizhi.git_2004.dao;
+
+import com.baizhi.git_2004.entity.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    List<User> queryAll();
+
+}
---
+++ src/main/java/com/baizhi/git_2004/entity/User.java
@@ -0,0 +1,9 @@
+package com.baizhi.git_2004.entity;
+
+public class User {
+
+    private String id;
+    private String name;
+    private Integer age;
+
+}
